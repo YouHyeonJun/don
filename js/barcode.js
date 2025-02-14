9cedfaa0ab7dbdd67e07ebe883740aaa134d58e1
YouHyeonJun <60587987+YouHyeonJun@users.noreply.github.com>
2025-02-14 20:33:05 +0900

    바코드 생성기

---
+++ js/barcode.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+  document.getElementById("userInput").addEventListener("keyup", function () {
+    const userInput = document.getElementById("userInput");
+    const value = UserInputSort(userInput.value);
+    BarcodeGeneration(value);
+  });
+};
+
+function UserInputSort(userInput) {
+  let value = userInput.split(" ");
+  return value;
+}
+
+function BarcodeGeneration(value) {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+  for (let i = 0; i < value.length; i++) {
+    const img = document.createElement("img");
+    img.id = `barcode${i}`;
+    container.appendChild(img);
+    JsBarcode("#barcode" + [i], value[i], {
+      format: "CODE128",
+      width: 2,
+      height: 80,
+      Margin: 10,
+      displayValue: true,
+    });
+  }
+}
